2df5f9f1deb39aad853f5cced03c887807036915
szy0syz <szy0syz@gmail.com>
2019-06-03 22:07:59 +0800

    Update [8-3] 始发终到站

--- train-ticket/src/index/App.jsx
+++ train-ticket/src/index/App.jsx
@@ -1,4 +1,5 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import './App.css';
 
@@ -10,29 +11,52 @@ import Submit from './Submit.jsx';
 
 // 注意：这里有自组装件的重渲染问题，必须使用useCallback包裹起来，这样不会在子组件里重复渲染
 
+import {
+  exchangeFromTo,
+  showCitySelector
+} from './actions'
+
 function App(props) {
+  const { from, to, dispatch } = props;
+
   const onBack = useCallback(() => {
     window.history.back();
   }, []);
+
+  const cbs = useMemo(() => {
+    return bindActionCreators({
+      exchangeFromTo,
+      showCitySelector,
+    }, dispatch);
+  }, [dispatch]);
 
   return (
     <div>
       <div className="header-wrapper">
         <Header title="火车票" onBack={onBack}></Header>
       </div>
-      <Journey></Journey>
-      <DepartDate></DepartDate>
-      <HighSpeed></HighSpeed>
-      <Submit></Submit>
+      <form 
+        action="/"
+        className="form"  
+      >
+        <Journey
+          from={from}
+          to={to}
+          {...cbs}
+        ></Journey>
+        <DepartDate></DepartDate>
+        <HighSpeed></HighSpeed>
+        <Submit></Submit>
+      </form>
     </div>
   )
 }
 
 export default connect(
   function mapStateToProps(state) {
-      return {};
+    return state;
   },
   function mapDispatchToProps(dispatch) {
-      return {};
+    return { dispatch };
   }
 )(App);
--- train-ticket/src/index/Journey.jsx
+++ train-ticket/src/index/Journey.jsx
@@ -1,6 +1,41 @@
 import React from 'react';
 import './Journey.css';
+import switchImg from './imgs/switch.svg';
 
 export default function Journey(props) {
-  return (<div>Journey</div>);
+  const { from, to, exchangeFromTo, showCitySelector } = props;
+  return (
+  <div className="journey">
+    <div 
+      className="journey-station"
+      onClick={()=> showCitySelector(true)}
+    >
+      <input
+        type="text"
+        readOnly
+        name="from"
+        value={from}
+        className="journey-input journey-from"
+      />
+    </div>
+    <div 
+      className="journey-switch"
+      onClick={()=> exchangeFromTo()}
+      >
+      <img src={switchImg} width="70" height="40" alt="siwtch" />
+    </div>
+    <div 
+      className="journey-station"
+      onClick={()=> showCitySelector(false)}
+    >
+    <input
+        type="text"
+        readOnly
+        name="to"
+        value={to}
+        className="journey-input journey-to"
+      />
+    </div>
+  </div>
+  );
 }
