2b87780007537ec1119d1588005f22f6b6aff491
szy0syz <szy0syz@gmail.com>
2019-06-09 23:50:18 +0800

    Update [9-3] URL解析与数据请求

--- train-ticket/src/query/App.jsx
+++ train-ticket/src/query/App.jsx
@@ -1,15 +1,150 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { connect } from 'react-redux';
+import URI from 'urijs';
+import dayjs from 'dayjs';
 
+import Header from '../common/Header.jsx';
 import Nav from '../common/Nav.jsx';
 import List from './List.jsx';
 import Bottom from './Bottom.jsx';
 
+import { h0 } from '../common/fp.js';
+
+import {
+  setFrom,
+  setTo,
+  setDepartDate,
+  setHighSpeed,
+  setSearchParsed,
+
+  setTrainList,
+  setTicketTypes,
+  setTrainTypes,
+  setDepartStations,
+  setArriveStations,
+
+} from './actions';
+
 import './App.css';
 
 function App(props) {
+  const {
+    from,
+    to,
+    departDate,
+    highSpeed,
+    searchParsed,
+    dispatch,
+    orderType,
+    onlyTickets,
+    isFiltersVisible,
+    ticketTypes,
+    trainTypes,
+    departStations,
+    arriveStations,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd,
+  } = props;
+
+  // 副作用：设置解析到的uri
+  useEffect(() => {
+    const queries = URI.parseQuery(window.location.search);
+
+    const {
+      from,
+      to,
+      date,
+      highSpeed,
+    } = queries;
+
+    dispatch(setFrom(from));
+    dispatch(setTo(to));
+    dispatch(setDepartDate(h0(dayjs(date).valueOf())));
+    dispatch(setHighSpeed(highSpeed === 'true'));
+
+    dispatch(setSearchParsed(true));
+  }, []);
+
+  // 副作用：根据获取的参数 拉取数据
+  useEffect(() => {
+    if (!searchParsed) { return; }
+
+    const url = new URI('/rest/query')
+      .setSearch('from', from)
+      .setSearch('to', to)
+      .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
+      .setSearch('highSpeed', highSpeed)
+      .setSearch('orderType', orderType)
+      .setSearch('onlyTickets', onlyTickets)
+      .setSearch('checkedTicketTypes', Object.keys(checkedTicketTypes).join())
+      .setSearch('checkedTrainTypes', Object.keys(checkedTrainTypes).join())
+      .setSearch('checkedDepartStations', Object.keys(checkedDepartStations).join())
+      .setSearch('checkedArriveStations', Object.keys(checkedArriveStations).join())
+      .setSearch('departTimeStart', departTimeStart)
+      .setSearch('departTimeEnd', departTimeEnd)
+      .setSearch('arriveTimeStart', arriveTimeStart)
+      .setSearch('arriveTimeEnd', arriveTimeEnd)
+      .toString();
+
+    fetch(url)
+      .then(response => response.json())
+      .then(res => {
+        const {
+          dataMap: {
+            directTrainInfo: {
+              trains,
+              filter: {
+                ticketType,
+                trainType,
+                depStation,
+                arrStation,
+              },
+            },
+          },
+        } = res;
+
+        dispatch(setTrainList(trains));
+        dispatch(setTicketTypes(ticketType));
+        dispatch(setTrainTypes(trainType));
+        dispatch(setDepartStations(depStation));
+        dispatch(setArriveStations(arrStation));
+      })
+
+  }, [
+      from,
+      to,
+      departDate,
+      highSpeed,
+      searchParsed,
+      orderType,
+      onlyTickets,
+      checkedTicketTypes,
+      checkedTrainTypes,
+      checkedDepartStations,
+      checkedArriveStations,
+      departTimeStart,
+      departTimeEnd,
+      arriveTimeStart,
+      arriveTimeEnd,
+    ]);
+
+  const onBack = useCallback(() => {
+    window.history.back();
+  }, []);
+
+  if (!searchParsed) { return null; }
+
   return (
     <div>
+      <div className="header-wrapper">
+        <Header title={`${from} → ${to}`} onBack={onBack} />
+      </div>
       <Nav />
       <List />
       <Bottom />
--- train-ticket/src/query/reducers.js
+++ train-ticket/src/query/reducers.js
@@ -25,229 +25,229 @@ import { ORDER_DEPART } from './constant';
 
 export default {
   from(state = null, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_FROM:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_FROM:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   to(state = null, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_TO:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_TO:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   departDate(state = Date.now(), action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_DEPART_DATE:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_DEPART_DATE:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   highSpeed(state = false, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_HIGH_SPEED:
-              return payload;
-          case ACTION_SET_CHECKED_TRAIN_TYPES:
-               const checkedTrainTypes = payload;
-               return Boolean(checkedTrainTypes[1] && checkedTrainTypes[5]);
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_HIGH_SPEED:
+        return payload;
+      case ACTION_SET_CHECKED_TRAIN_TYPES:
+        const checkedTrainTypes = payload;
+        return Boolean(checkedTrainTypes[1] && checkedTrainTypes[5]);
+      default:
+    }
 
-      return state;
+    return state;
   },
   trainList(state = [], action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_TRAIN_LIST:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_TRAIN_LIST:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   orderType(state = ORDER_DEPART, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_ORDER_TYPE:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_ORDER_TYPE:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   onlyTickets(state = false, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_ONLY_TICKETS:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_ONLY_TICKETS:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   ticketTypes(state = [], action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_TICKET_TYPES:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_TICKET_TYPES:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   checkedTicketTypes(state = {}, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_CHECKED_TICKET_TYPES:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_CHECKED_TICKET_TYPES:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   trainTypes(state = [], action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_TRAIN_TYPES:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_TRAIN_TYPES:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   checkedTrainTypes(state = {}, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_CHECKED_TRAIN_TYPES:
-              return payload;
-          case ACTION_SET_HIGH_SPEED:
-              const highSpeed = payload;
-              const newCheckedTrainTypes = {...state};
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_CHECKED_TRAIN_TYPES:
+        return payload;
+      case ACTION_SET_HIGH_SPEED:
+        const highSpeed = payload;
+        const newCheckedTrainTypes = { ...state };
 
-              if (highSpeed) {
-                  newCheckedTrainTypes[1] = true;
-                  newCheckedTrainTypes[5] = true;
-              } else {
-                  delete newCheckedTrainTypes[1];
-                  delete newCheckedTrainTypes[5];
-              }
+        if (highSpeed) {
+          newCheckedTrainTypes[1] = true;
+          newCheckedTrainTypes[5] = true;
+        } else {
+          delete newCheckedTrainTypes[1];
+          delete newCheckedTrainTypes[5];
+        }
 
-              return newCheckedTrainTypes;
-          default:
-      }
+        return newCheckedTrainTypes;
+      default:
+    }
 
-      return state;
+    return state;
   },
   departStations(state = [], action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_DEPART_STATIONS:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_DEPART_STATIONS:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   checkedDepartStations(state = {}, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_CHECKED_DEPART_STATIONS:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_CHECKED_DEPART_STATIONS:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   arriveStations(state = [], action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_ARRIVE_STATIONS:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_ARRIVE_STATIONS:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   checkedArriveStations(state = {}, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_CHECKED_ARRIVE_STATIONS:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_CHECKED_ARRIVE_STATIONS:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   departTimeStart(state = 0, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_DEPART_TIME_START:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_DEPART_TIME_START:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   departTimeEnd(state = 24, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_DEPART_TIME_END:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_DEPART_TIME_END:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   arriveTimeStart(state = 0, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_ARRIVE_TIME_START:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_ARRIVE_TIME_START:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   arriveTimeEnd(state = 24, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_ARRIVE_TIME_END:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_ARRIVE_TIME_END:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   isFiltersVisible(state = false, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_IS_FILTERS_VISIBLE:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_IS_FILTERS_VISIBLE:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
   searchParsed(state = false, action) {
-      const { type, payload } = action;
-      switch(type) {
-          case ACTION_SET_SEARCH_PARSED:
-              return payload;
-          default:
-      }
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_SEARCH_PARSED:
+        return payload;
+      default:
+    }
 
-      return state;
+    return state;
   },
 };
