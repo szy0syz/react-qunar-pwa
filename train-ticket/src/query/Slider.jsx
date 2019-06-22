932e230748c099c27526de5e48471d4171b6d930
szy0syz <szy0syz@gmail.com>
2019-06-22 09:22:26 +0800

    Add Slider

--- train-ticket/src/query/Slider.jsx
+++ train-ticket/src/query/Slider.jsx
@@ -1,10 +1,4 @@
-import React, {
-  memo,
-  useState,
-  useMemo,
-  useRef,
-  useEffect,
-} from 'react';
+import React, { memo, useState, useMemo, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import leftPad from 'left-pad';
 import useWinSize from '../common/useWinSize';
@@ -12,11 +6,11 @@ import './Slider.css';
 
 const Slider = memo(function Slider(props) {
   const {
-      title,
-      currentStartHours,
-      currentEndHours,
-      onStartChanged,
-      onEndChanged,
+    title,
+    currentStartHours,
+    currentEndHours,
+    onStartChanged,
+    onEndChanged,
   } = props;
 
   const winSize = useWinSize();
@@ -33,169 +27,160 @@ const Slider = memo(function Slider(props) {
   const prevCurrentStartHours = useRef(currentStartHours);
   const prevCurrentEndHours = useRef(currentEndHours);
 
-  const [start, setStart]  = useState(() => currentStartHours / 24 * 100);
-  const [end, setEnd]  = useState(() => currentEndHours / 24 * 100);
+  const [start, setStart] = useState(() => (currentStartHours / 24) * 100);
+  const [end, setEnd] = useState(() => (currentEndHours / 24) * 100);
 
   if (prevCurrentStartHours.current !== currentStartHours) {
-      setStart((currentStartHours / 24) * 100);
-      prevCurrentStartHours.current = currentStartHours;
+    setStart((currentStartHours / 24) * 100);
+    prevCurrentStartHours.current = currentStartHours;
   }
 
   if (prevCurrentEndHours.current !== currentEndHours) {
-      setEnd((currentEndHours / 24) * 100);
-      prevCurrentEndHours.current = currentEndHours;
+    setEnd((currentEndHours / 24) * 100);
+    prevCurrentEndHours.current = currentEndHours;
   }
 
   const startPercent = useMemo(() => {
-      if (start > 100) {
-          return 100;
-      }
+    if (start > 100) {
+      return 100;
+    }
 
-      if (start < 0) {
-          return 0;
-      }
+    if (start < 0) {
+      return 0;
+    }
 
-      return start;
+    return start;
   }, [start]);
 
   const endPercent = useMemo(() => {
-      if (end > 100) {
-          return 100;
-      }
+    if (end > 100) {
+      return 100;
+    }
 
-      if (end < 0) {
-          return 0;
-      }
+    if (end < 0) {
+      return 0;
+    }
 
-      return end;
+    return end;
   }, [end]);
 
   const startHours = useMemo(() => {
-      return Math.round(startPercent * 24 / 100);
+    return Math.round((startPercent * 24) / 100);
   }, [startPercent]);
 
   const endHours = useMemo(() => {
-      return Math.round(endPercent * 24 / 100);
+    return Math.round((endPercent * 24) / 100);
   }, [endPercent]);
 
   const startText = useMemo(() => {
-      return leftPad(startHours, 2, '0') + ':00';
+    return leftPad(startHours, 2, '0') + ':00';
   }, [startHours]);
 
   const endText = useMemo(() => {
-      return leftPad(endHours, 2, '0') + ':00';
+    return leftPad(endHours, 2, '0') + ':00';
   }, [endHours]);
 
   function onStartTouchBegin(e) {
-      const touch = e.targetTouches[0];
-      lastStartX.current = touch.pageX;
+    const touch = e.targetTouches[0];
+    lastStartX.current = touch.pageX;
   }
 
   function onEndTouchBegin(e) {
-      const touch = e.targetTouches[0];
-      lastEndX.current = touch.pageX;
+    const touch = e.targetTouches[0];
+    lastEndX.current = touch.pageX;
   }
 
   function onStartTouchMove(e) {
-      const touch = e.targetTouches[0];
-      const distance = touch.pageX - lastStartX.current;
-      lastStartX.current = touch.pageX;
+    const touch = e.targetTouches[0];
+    const distance = touch.pageX - lastStartX.current;
+    lastStartX.current = touch.pageX;
 
-      setStart(start => start + (distance / rangeWidth.current) * 100);
+    setStart(start => start + (distance / rangeWidth.current) * 100);
   }
 
   function onEndTouchMove(e) {
-      console.log('!~~e',e)
-      const touch = e.targetTouches[0];
-      const distance = touch.pageX - lastEndX.current;
-      lastEndX.current = touch.pageX;
+    const touch = e.targetTouches[0];
+    const distance = touch.pageX - lastEndX.current;
+    lastEndX.current = touch.pageX;
 
-      setEnd(end => end + (distance / rangeWidth.current) * 100);
+    setEnd(end => end + (distance / rangeWidth.current) * 100);
   }
 
   useEffect(() => {
-      rangeWidth.current = parseFloat(
-          window.getComputedStyle(range.current).width,
-      );
+    rangeWidth.current = parseFloat(
+      window.getComputedStyle(range.current).width,
+    );
   }, [winSize.width]);
 
   useEffect(() => {
-      startHandle.current.addEventListener(
-          'touchstart',
-          onStartTouchBegin,
-          false,
-      );
-      startHandle.current.addEventListener(
-          'touchmove',
-          onStartTouchMove,
-          false,
-      );
-      endHandle.current.addEventListener(
-          'touchstart',
-          onEndTouchBegin,
-          false,
-      );
-      endHandle.current.addEventListener(
-          'touchmove',
-          onEndTouchMove,
-          false,
-      );
+    startHandle.current.addEventListener(
+      'touchstart',
+      onStartTouchBegin,
+      false,
+    );
+    startHandle.current.addEventListener('touchmove', onStartTouchMove, false);
+    endHandle.current.addEventListener('touchstart', onEndTouchBegin, false);
+    endHandle.current.addEventListener('touchmove', onEndTouchMove, false);
 
-      return () => {
+    return () => {
       startHandle.current.removeEventListener(
-          'touchstart',
-          onStartTouchBegin,
-          false,
+        'touchstart',
+        onStartTouchBegin,
+        false,
       );
       startHandle.current.removeEventListener(
-          'touchmove',
-          onStartTouchMove,
-          false,
+        'touchmove',
+        onStartTouchMove,
+        false,
       );
       endHandle.current.removeEventListener(
-          'touchstart',
-          onEndTouchBegin,
-          false,
+        'touchstart',
+        onEndTouchBegin,
+        false,
       );
-      endHandle.current.removeEventListener(
-          'touchmove',
-          onEndTouchMove,
-          false,
-      );
-      };
+      endHandle.current.removeEventListener('touchmove', onEndTouchMove, false);
+    };
   });
 
   useEffect(() => {
-      onStartChanged(startHours);
+    onStartChanged(startHours);
   }, [startHours]);
 
   useEffect(() => {
-      onEndChanged(endHours);
+    onEndChanged(endHours);
   }, [endHours]);
 
   return (
-      <div className="option">
-          <h3>{ title }</h3>
-          <div className="range-slider">
-              <div className="slider" ref={range}>
-                  <div className="slider-range" style={{
-                      left: startPercent + '%',
-                      width: endPercent - startPercent + '%',
-                  }}>
-                  </div>
-                  <i ref={startHandle} className="slider-handle" style={{
-                      left: startPercent + '%',
-                  }}>
-                      <span>{ startText }</span>
-                  </i>
-                  <i ref={endHandle} className="slider-handle" style={{
-                      left: endPercent + '%',
-                  }}>
-                      <span>{ endText }</span>
-                  </i>
-              </div>
-          </div>
+    <div className='option'>
+      <h3>{title}</h3>
+      <div className='range-slider'>
+        <div className='slider' ref={range}>
+          <div
+            className='slider-range'
+            style={{
+              left: startPercent + '%',
+              width: endPercent - startPercent + '%',
+            }}
+          />
+          <i
+            ref={startHandle}
+            className='slider-handle'
+            style={{
+              left: startPercent + '%',
+            }}>
+            <span>{startText}</span>
+          </i>
+          <i
+            ref={endHandle}
+            className='slider-handle'
+            style={{
+              left: endPercent + '%',
+            }}>
+            <span>{endText}</span>
+          </i>
+        </div>
       </div>
+    </div>
   );
 });
 
